b7782f900af6db39f1f50137c58d085c0fe7692a
Richard Hennessey III <Rick@Richards-iMac.local>
2020-06-17 14:57:47 -0400

    added context import to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
+import { ThemeContextProvider } from "./themeContext";
 
 import Header from "./Header"
 import Button from "./Button"
 
 function App() {
+ 
   return (
     <div>
       <ThemeContextProvider>
